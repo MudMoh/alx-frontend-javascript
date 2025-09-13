a94dcaf0992dd6384dbb817107ae221cc58101f8
MudMoh <mudesir@gmail.com>
2025-09-13 15:43:10 +0300

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,7 +35,7 @@ interface printTeacherFunction {
 }
 
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName[0]}. ${lastName}`;
+  return `${firstName}. ${lastName}`;
 }
 
 const printTeacherFunction: printTeacherFunction = printTeacher;
